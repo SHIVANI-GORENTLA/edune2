2a2d465824f0c88f06d5ace872a6286952a3cce2
SHIVANI-GORENTLA <shivanigorentla18@gmail.com>
2025-09-16 10:59:28 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,63 @@
+#for i in range(1,10,1):
+   # print("Shivani",i)
+"""
+for i in range(1,20):
+    if(i%2==0):
+        print(i)
+        
+
+for i in range(0,21,2):
+    print(i)
+
+for i in range(0,21,3):
+    print(i)
+
+a="harekrishna"
+for i in a:
+    print(i)
+
+a="harekrishna"
+for i in a:
+    print(i,"a")
+
+for i in range(1,51):
+    if(i%2==0 and i%3==0):
+        print(i)
+
+for i in range(0,51,6):
+    print(i)
+
+for i in range(1,51):
+    if(i%2==0 and i%5==0):
+        print(i)
+
+n=int(input())
+sum=0
+for i in range(n):
+    sum=sum+i
+print(sum)
+
+fact=int(input())
+for i in range(1,fact):
+    fact=fact*i
+    print(fact)
+    
+for i in range(3077,3103,1):
+    print(chr(i),end=" ")
+    
+s=input()
+asciivalue=ord(chr)
+print(asciivalue)
+
+x=input("enter string:")
+for i in x:
+    print("ascii value of",i,ord(i))
+i=0
+while i<10:
+    print("SHIVANI")
+    i+=1
+"""
+i=2
+while(i<=10):
+    print(i)
+    i=i+2
